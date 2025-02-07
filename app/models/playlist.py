5ad895a66e18f7aa3ff029c0da37720685c1d0ed
fayfan <sbaltz98@gmail.com>
2025-02-07 11:22:58 -0600

    Change likes model to join table, add artist_name to User model, add to_dict function to Song model (commented out), & change duration to string

--- app/models/playlist.py
+++ app/models/playlist.py
@@ -2,14 +2,19 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 from datetime import datetime
 from .playlist_songs import playlist_songs
 
+
 class Playlist(db.Model):
     __tablename__ = "playlists"
 
     if environment == "production":
-        __table_args__ = {'schema': SCHEMA}
+        __table_args__ = {"schema": SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id"), ondelete="CASCADE"), nullable=False)
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey(add_prefix_for_prod("users.id"), ondelete="CASCADE"),
+        nullable=False,
+    )
     image_url = db.Column(db.String(2083))
     name = db.Column(db.String(255), nullable=False)
     description = db.Column(db.String(255), nullable=False)
@@ -17,5 +22,6 @@ class Playlist(db.Model):
     updated_at = db.Column(db.DateTime, default=datetime.today, onupdate=datetime.today)
 
     user = db.relationship("User", back_populates="playlists")
-
-    songs = db.relationship("Song", secondary=playlist_songs, back_populates="playlists")
+    songs = db.relationship(
+        "Song", secondary=playlist_songs, back_populates="playlists"
+    )
