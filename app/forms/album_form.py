e7dc93fe9caa0205a46e2221b46b5ff9bb7fedd7
tom-moosbrugger <tamoosbrugger@gmail.com>
2025-02-08 11:00:23 -0800

    fixed album form

--- app/forms/album_form.py
+++ app/forms/album_form.py
@@ -2,12 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField
 from wtforms.validators import DataRequired, URL, ValidationError
 
-def validate_release_year(self, field):
-    if not field.data:
-        raise ValidationError("Release year must be a valid integer.")
-
 class AlbumForm(FlaskForm):
     title = StringField('title', validators=[DataRequired()])
     image_url = StringField('image_url', validators=[DataRequired(), URL()])
-    release_year = IntegerField('release_year', validators=[validate_release_year, DataRequired()])
+    release_year = IntegerField('release_year', validators=[DataRequired()])
     
