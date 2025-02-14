ff200687d8bd0d029390d91df464b4e9255cecd4
MehwishHanif <mehwish.hanif90@gmail.com>
2025-02-14 10:11:30 -0800

    added styling for plylist details

--- react-vite/src/components/LikesDetails/LikesDetails.jsx
+++ react-vite/src/components/LikesDetails/LikesDetails.jsx
@@ -1,5 +1,6 @@
 import { useSelector } from "react-redux";
 import LikedSongs from "./LikedSongs";
+import {  useNavigate } from "react-router-dom";
 
 const LikesDetails = () => {
     
@@ -7,7 +8,10 @@ const LikesDetails = () => {
     const likedSongsCount = likedSongIds?.length;
     const user = useSelector((state) => state.session.user);
     const LikeUrl = "https://img.freepik.com/free-photo/minimalist-heart-mockup_64049-79.jpg";
+    const navigate = useNavigate();
     
+    if (!user) navigate("/");
+
     return(
         <div className="playlist-details">
             <div className="playlist-header">
